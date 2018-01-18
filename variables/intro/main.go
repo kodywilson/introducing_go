5b5b1e32cc8f43b2c3d3d4404ab4293b8ffadc33
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-01-18 15:21:49 -0800

    finished up the intro to variables section

--- variables/intro/main.go
+++ variables/intro/main.go
@@ -5,4 +5,21 @@ import "fmt"
 func main() {
 	var x string = "Hello, World"
 	fmt.Println(x)
+	var y string
+	y = "Nice to meet you"
+	fmt.Println(y)
+	var z string
+	z = "first"
+	fmt.Println(z)
+	z = "second"
+	fmt.Println(z)
+	z = z + " third"
+	fmt.Println(z)
+	z += " fourth"
+	fmt.Println(z)
+	var a string = "hello"
+	var b string = "world"
+	fmt.Println(a == b)
+	b = "hello"
+	fmt.Println(a == b)
 }
