238c1b432ffd6566a69ec088ef7c5bd8268e6db3
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-01-23 15:48:15 -0800

    Finished chapter 4 exercises

---
+++ control_sctructures/exercises/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 10
+	if i > 10 {
+		fmt.Println("Big")
+	} else {
+		fmt.Println("Small")
+	}
+	for i := 1; i < 101; i++ {
+		if i%3 == 0 {
+			fmt.Println(i, "Evenly divided by 3!")
+		}
+	}
+	// FizzBuzz!
+	for i := 1; i < 101; i++ {
+		if i%3 == 0 {
+			if i%5 == 0 {
+				fmt.Println(i, "FizzBuzz")
+				continue
+			} else {
+				fmt.Println(i, "Fizz")
+			}
+		}
+		if i%5 == 0 {
+			if i%3 == 0 {
+				fmt.Println(i, "FizzBuzz")
+				continue
+			} else {
+				fmt.Println(i, "Buzz")
+			}
+		}
+	}
+}
