ed7b3df740f6b91274d497851b909f7bd6c5ca14
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-01-30 07:49:41 -0800

    first way to create map

--- array_slice_map/maps/main.go
+++ array_slice_map/maps/main.go
@@ -12,10 +12,26 @@ func main() {
 	fmt.Println(y[1])
 
 	z := make(map[int]int)
-	for i := 0; i < 10; i++ {
+	for i := 1; i < 11; i++ {
 		z[i] = i * 10
 		fmt.Println("The length of z is: ", len(z))
 		fmt.Println(z[i])
+	}
+
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+
+	if name, ok := elements["Ne"]; ok {
+		fmt.Println(name, ok)
 	}
 
 }
