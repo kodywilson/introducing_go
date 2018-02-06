c4688f3a0bc925a678ec138dffa95a597374b6f4
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-02-05 17:23:35 -0800

    a working function

--- functions/second/main.go
+++ functions/second/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func average(xs []float64) float64 {
-	panic("Not Implemented")
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
 }
 
 func main() {
-	xs := []float64{98, 93, 77, 82, 83}
+	xs := []float64{98, 93, 77, 82, 83, 1000}
 
 	total := 0.0
 	for _, v := range xs {
