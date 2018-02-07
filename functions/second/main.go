b668360a3f84a6b31bfa95e621ed7f4a737f9fbf
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-02-07 15:02:20 -0800

    finished first function

--- functions/second/main.go
+++ functions/second/main.go
@@ -12,11 +12,5 @@ func average(xs []float64) float64 {
 
 func main() {
 	xs := []float64{98, 93, 77, 82, 83, 1000, 24, 100000}
-
-	total := 0.0
-	for _, v := range xs {
-		total += v
-	}
-	fmt.Println(total / float64(len(xs)))
-	average(xs)
+	fmt.Println(average(xs))
 }
