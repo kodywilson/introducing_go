920c9e8cbf40698b1b3f21a4178cef9c74bcb367
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-02-08 10:00:38 -0800

    different function types

--- functions/second/main.go
+++ functions/second/main.go
@@ -10,7 +10,28 @@ func average(xs []float64) float64 {
 	return total / float64(len(xs))
 }
 
+func f(x int) {
+	fmt.Println(x)
+}
+
+func f1() int {
+	return f2()
+}
+
+func f2() int {
+	return 1
+}
+
+func f3() (int, int) {
+	return 5, 6
+}
+
 func main() {
 	xs := []float64{98, 93, 77, 82, 83, 1000, 24, 100000}
 	fmt.Println(average(xs))
+	x := 5
+	f(x)
+	fmt.Println(f1())
+	x, y := f3()
+	fmt.Println(x, y)
 }
