62ad90a31c8ac7b6781bef35389e5c15d9d7fdc4
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-02-26 12:22:38 -0800

    go by example - finishing if_else

--- go_by_example/if_else/main.go
+++ go_by_example/if_else/main.go
@@ -13,12 +13,13 @@ func main() {
 	if 8%4 == 0 {
 		fmt.Println("8 is evenly divisible by 4")
 	}
-
-	if num := 9; num < 0 {
-		fmt.Println(num, "is negative")
-	} else if num < 10 {
-		fmt.Println(num, "has 1 digit")
-	} else {
-		fmt.Println(num, "has multiple digits")
+	for num := -10; num < 11; num++ {
+		if num < 0 {
+			fmt.Println(num, "is negative")
+		} else if num < 10 {
+			fmt.Println(num, "has 1 digit")
+		} else {
+			fmt.Println(num, "has multiple digits")
+		}
 	}
 }
