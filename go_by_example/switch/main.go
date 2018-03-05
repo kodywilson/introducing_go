0aac25fb69f16d839af3c0c89ebbd7eb9545637a
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-03-05 14:32:09 -0800

    go by example - switch and types

--- go_by_example/switch/main.go
+++ go_by_example/switch/main.go
@@ -31,4 +31,18 @@ func main() {
 	default:
 		fmt.Println("It's after noon")
 	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
