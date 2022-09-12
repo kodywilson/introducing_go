ec0db519eb4ece20fd1bfc75b6e7b312cae12b62
Kody Wilson <kody.wilson@oracle.com>
2022-09-11 21:09:25 -0700

    error handling tutorial

--- go_tutorials_official/hello/hello.go
+++ go_tutorials_official/hello/hello.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "example.com/greetings"
+	"example.com/greetings"
+	"fmt"
+	"log"
 )
 
 func main() {
-  // Get a greeting message and print it.
-  message := greetings.Hello("Bob")
+	// Set properties of the predefined logger, including the long entry prefix and a flag to
+	// disable printing the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message to the console
 	fmt.Println(message)
 }
