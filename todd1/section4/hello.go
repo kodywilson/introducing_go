1249d1e8c5086a9429abae9c5009f23767a3d6a3
Kody Wilson <kodywilson@gmail.com>
2022-09-14 17:30:08 -0700

    more todd class

--- todd1/section4/hello.go
+++ todd1/section4/hello.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	x := 42
 	fmt.Println("Hello Everyone!")
 	foo()
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
 	fmt.Println("something more")
 
 	for i := 0; i < 10; i++ {
