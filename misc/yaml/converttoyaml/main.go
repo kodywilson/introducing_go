f20d1a57954361657336f1677e19cef5786baee5
Kody Wilson <kodywilson@gmail.com>
2022-09-15 14:45:06 -0700

    working with yaml in Go

--- misc/yaml/converttoyaml/main.go
+++ misc/yaml/converttoyaml/main.go
@@ -3,29 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
 type Car struct {
-	TopSpeed   int
-	Name       string
-	Cool       bool
-	Passengers []string
+	TopSpeed   int      `yaml:"topspeed"`
+	Name       string   `yaml:"name"`
+	Cool       bool     `yaml:"cool"`
+	Passengers []string `yaml:"passengers"`
 }
 
 func main() {
-	c := Car{
-		TopSpeed:   60,
-		Name:       "Mirthmobile",
-		Cool:       true,
-		Passengers: []string{"garth", "wayne"},
-	}
-
-	out, err := yaml.Marshal(c)
+	// Load the file; returns []byte
+	f, err := os.ReadFile("yammy.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(out))
+	// Create an empty Car to be are target of unmarshalling
+	var c Car
+
+	// Unmarshal our input YAML file into empty Car (var c)
+	if err := yaml.Unmarshal(f, &c); err != nil {
+		log.Fatal(err)
+	}
+
+	// Print out the new struct
+	fmt.Printf("%+v\n", c)
 }
