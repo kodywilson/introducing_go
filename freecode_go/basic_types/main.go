492becaaa4ecf2adb9e226d2721537e8b7811b3e
Kody Wilson <kodywilson@gmail.com>
2022-09-16 16:32:21 -0700

    basic types

--- freecode_go/basic_types/main.go
+++ freecode_go/basic_types/main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	// bools and ints
 	var t bool = true
 	n := 42
 	o := -42
@@ -17,4 +18,23 @@ func main() {
 	fmt.Printf("n = %v and is a %T\n", n, n)
 	fmt.Printf("o = %v and is a %T\n", o, o)
 	fmt.Printf("p = %v and is a %T\n", p, p)
+
+	// and or xor
+	x := 10             // 1010
+	y := 3              // 0011
+	fmt.Println(x & y)  // 0010
+	fmt.Println(x | y)  // 1011
+	fmt.Println(x ^ y)  // 1001
+	fmt.Println(x &^ y) // 0100
+
+	// bit shifting
+	z := 8              // 00001000
+	fmt.Println(z << 3) // 01000000 = 64 // 2^3 * 2^3 = 2^6 = 64
+	fmt.Println(z >> 3) // 00000001 = 1  // 2^3 / 2^3 = 2^0 = 1
+
+	// floats
+	g := 3.14
+	g = 13.7e72
+	g = 2.1e14
+	fmt.Printf("%v, %T\n", g, g)
 }
