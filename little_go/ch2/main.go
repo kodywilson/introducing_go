8e549433d4dd1344acc8f9e690113f9881b057dc
Kody Wilson <kody.wilson@oracle.com>
2022-09-16 17:34:36 -0700

    little go book structs

---
+++ little_go/ch2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Player struct {
+	Name string
+	Str int
+}
+
+func main() {
+	bob := Player{
+		Name: "Bob",
+		Str: 16,
+	}
+	fmt.Println(bob.Name)
+
+	darrel := Player{Name: "Darrel"}
+	darrel.Str = 12
+	fmt.Println(darrel.Name, darrel.Str)
+
+	Super(darrel)
+	fmt.Println(darrel.Name, darrel.Str)
+}
+
+func Super(p Player) {
+	p.Str += 2
+}
