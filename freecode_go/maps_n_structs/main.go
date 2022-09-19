4dc383906c8a4de6a1b86d9092d9adb563da1be8
Kody Wilson <kodywilson@gmail.com>
2022-09-19 16:01:56 -0700

    maps and structs in Go

--- freecode_go/maps_n_structs/main.go
+++ freecode_go/maps_n_structs/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// Create map of states with population
 	statePopulations := map[string]int{
 		"California":   39250017,
 		"Texas":        27862596,
@@ -14,4 +15,16 @@ func main() {
 	}
 	fmt.Println(statePopulations)
 	fmt.Printf("The population of Texas is %v\n", statePopulations["Texas"])
+	statePopulations["Georgia"] = 10310371 // add a new state
+	fmt.Println(statePopulations)
+	delete(statePopulations, "Georgia") // remove state
+	fmt.Println(statePopulations)
+	_, ok := statePopulations["Ohio"] // check if key is present
+	fmt.Println(ok)
+	fmt.Println(len(statePopulations))
+	// maps pass by reference
+	sp := statePopulations
+	delete(sp, "Ohio") // change sp and you change statePopulations
+	fmt.Println(sp)
+	fmt.Println(statePopulations)
 }
