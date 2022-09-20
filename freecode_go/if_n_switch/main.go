0fcfeedb19b2b1de61da9cb9a8b5de0022cb63b4
Kody Wilson <kodywilson@gmail.com>
2022-09-20 09:55:56 -0700

    control flow in Go

--- freecode_go/if_n_switch/main.go
+++ freecode_go/if_n_switch/main.go
@@ -8,13 +8,24 @@ func main() {
 	}
 	number := 50
 	guess := 30
-	if guess < number {
-		fmt.Println("Too low")
+	if guess < 1 || guess > 100 {
+		fmt.Println("The guess must be between 1 and 100!")
+	} else {
+		if guess < number {
+			fmt.Println("Too low")
+		}
+		if guess > number {
+			fmt.Println("Too high")
+		}
+		if guess == number {
+			fmt.Println("You got it!")
+		}
+		fmt.Println(number <= guess, number >= guess, number != guess)
+		fmt.Println(returnTrue())
 	}
-	if guess > number {
-		fmt.Println("Too high")
-	}
-	if guess == number {
-		fmt.Println("You got it!")
-	}
+}
+
+func returnTrue() bool {
+	fmt.Println("returning true")
+	return true
 }
