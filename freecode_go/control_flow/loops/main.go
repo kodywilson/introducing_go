179c7803604dcef637edd97c0759a02e2727296b
Kody Wilson <kodywilson@gmail.com>
2022-09-20 16:20:38 -0700

    loops in Go

--- freecode_go/control_flow/loops/main.go
+++ freecode_go/control_flow/loops/main.go
@@ -5,9 +5,11 @@ import (
 )
 
 func main() {
+	// simple loop
 	for i := 0; i < 5; i++ {
 		fmt.Println(i)
 	}
+	// can have more than one variable in initializer
 	for i, j := 0, 0; i < 5 && j <= 4; i, j = i+1, j+2 {
 		fmt.Println(i, j)
 	}
@@ -17,5 +19,28 @@ func main() {
 		} else {
 			fmt.Printf("%v is odd!\n", i)
 		}
+	}
+	// counter initialized outside loop (still need the first ;)
+	counter := 0
+	for ; counter < 5; counter++ {
+		fmt.Printf("counter value in the loop is %v\n", counter)
+	}
+	fmt.Printf("counter value outside the loop is %v\n", counter)
+
+	// other ways to break out of loop
+	for {
+		if counter > 8 {
+			break
+		}
+		fmt.Printf("Now counter is equal to %v\n", counter)
+		counter++
+	}
+
+	// continue statement // only print odd numbers in this example
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
 	}
 }
