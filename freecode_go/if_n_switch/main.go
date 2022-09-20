e30abfe2dabe52eff439903ea972d52fe2907c48
Kody Wilson <kodywilson@gmail.com>
2022-09-20 11:28:31 -0700

    control flow in Go

--- freecode_go/if_n_switch/main.go
+++ freecode_go/if_n_switch/main.go
@@ -35,13 +35,22 @@ func main() {
 	}
 
 	// switch statements
-	switch 2 {
-	case 1:
-		fmt.Println("one")
-	case 2:
+	switch i := 2 + 3; i {
+	case 1, 5, 10:
+		fmt.Println("one, five, or ten")
+	case 2, 4, 6:
 		fmt.Println("two")
 	default:
-		fmt.Println("not one or two")
+		fmt.Println("not matched")
+	}
+	j := 10
+	switch {
+	case j <= 10:
+		fmt.Println("less than or equal to ten")
+	case j <= 20:
+		fmt.Println("less than or equal to 20")
+	default:
+		fmt.Println("greater than twenty")
 	}
 }
 
