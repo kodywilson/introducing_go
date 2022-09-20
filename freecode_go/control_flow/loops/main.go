bf4fe73153335d0b40566666866acbaf01b6398b
Kody Wilson <kodywilson@gmail.com>
2022-09-20 16:07:59 -0700

    loops in Go

--- freecode_go/control_flow/loops/main.go
+++ freecode_go/control_flow/loops/main.go
@@ -8,4 +8,14 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println(i)
 	}
+	for i, j := 0, 0; i < 5 && j <= 4; i, j = i+1, j+2 {
+		fmt.Println(i, j)
+	}
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%v is even!\n", i)
+		} else {
+			fmt.Printf("%v is odd!\n", i)
+		}
+	}
 }
