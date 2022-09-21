8ce3e7eaae530b5531c06355a96e8bcaf609be22
Kody Wilson <kodywilson@gmail.com>
2022-09-21 12:54:12 -0700

    functions in Go

--- freecode_go/functions/main.go
+++ freecode_go/functions/main.go
@@ -5,9 +5,21 @@ import (
 )
 
 func main() {
-	sayMessage("Hello Go")
+	// for i := 0; i < 5; i++ {
+	// 	sayMessage("Hello Go!", i)
+	// }
+	greeting := "Hello"
+	name := "Bob"
+	sayGreeting(greeting, name)
 }
 
-func sayMessage(msg string) {
+func sayMessage(msg string, idx int) {
 	fmt.Println(msg)
+	fmt.Println("The value of the index is", idx)
+}
+
+func sayGreeting(greeting, name string) {
+	fmt.Println(greeting, name)
+	name = "Ted"
+	fmt.Println(name)
 }
