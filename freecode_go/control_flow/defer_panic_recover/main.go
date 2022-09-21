3d4c450571f22da0b0b6ffea0fac86bf4066fa30
Kody Wilson <kodywilson@gmail.com>
2022-09-20 17:04:11 -0700

    control flow - defer, panic, recover

--- freecode_go/control_flow/defer_panic_recover/main.go
+++ freecode_go/control_flow/defer_panic_recover/main.go
@@ -9,17 +9,29 @@ import (
 
 func main() {
 	fmt.Println("start")
-	defer fmt.Println("middle")
+	defer fmt.Println("middle") // will run after main ends
+	// defer goes last in first out order
 	fmt.Println("end")
 
 	res, err := http.Get("http://www.google.com/robots.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer res.Body.Close() // be careful with defer here if looping
 	robots, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", robots)
+	fmt.Printf("%s", robots[:100]) // use [:100] to print part of text
+
+	// panic
+	//panic("something terrible happened")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Go!"))
+	})
+	err2 := http.ListenAndServe(":8085", nil)
+	if err2 != nil {
+		panic(err2.Error())
+	}
 }
