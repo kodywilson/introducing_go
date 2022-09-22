f63828c15bd90e5c982ba01fb2c200169816c9a5
Kody Wilson <kodywilson@gmail.com>
2022-09-22 16:09:18 -0700

    single threaded app from goroutines

--- freecode_go/goroutines/main.go
+++ freecode_go/goroutines/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
-	"time"
 )
 
 var wg = sync.WaitGroup{}
@@ -13,35 +12,36 @@ var m = sync.RWMutex{}
 
 func main() {
 	runtime.GOMAXPROCS(100)
-	wg.Add(1)
-	go sayHello()
-	time.Sleep(100 * time.Microsecond)
+	// wg.Add(1)
+	// m.RLock()
+	// go sayHello()
 
-	var msg = "World"
-	wg.Add(1)
-	go func(msg string) {
-		fmt.Println(msg)
-		wg.Done()
-	}(msg)
-	msg = "Goodbye"
+	// var msg = "World"
+	// wg.Add(1)
+	// m.RLock()
+	// go func(msg string) {
+	// 	fmt.Println(msg)
+	// 	wg.Done()
+	// }(msg)
+	// msg = "Goodbye"
 	// more advanced example
 	for i := 0; i < 10; i++ {
 		wg.Add(2)
+		m.RLock()
 		go sayHello()
+		m.Lock()
 		go incremement()
 	}
 	wg.Wait()
 }
 
 func sayHello() {
-	m.RLock()
 	fmt.Printf("Hello %v\n", counter)
 	m.RUnlock()
 	wg.Done()
 }
 
 func incremement() {
-	m.Lock()
 	counter++
 	m.Unlock()
 	wg.Done()
