73642ddd1a0cf78e587888892147227eff16dac1
Kody Wilson <kodywilson@gmail.com>
2022-09-22 15:24:39 -0700

    working on booking app

--- nana_golang/booking-app/main.go
+++ nana_golang/booking-app/main.go
@@ -13,4 +13,11 @@ func main() {
 	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+	//ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %s booked %d tickets.\n", userName, userTickets)
+
 }
