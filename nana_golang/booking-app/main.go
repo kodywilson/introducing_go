216a1a00212d0457fa4998dbb5700b4cb4ac9cc5
Kody Wilson <kodywilson@gmail.com>
2022-09-22 14:05:06 -0700

    format output with Go

--- nana_golang/booking-app/main.go
+++ nana_golang/booking-app/main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Print("Hello World\n")
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	remainingTickets := 50
+
+	fmt.Printf("Welcome to %s booking application\n", conferenceName)
+	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets here to attend")
+
 }
