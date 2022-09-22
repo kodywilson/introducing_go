8f47b4d36392d892f03e9f2d875a60846920e546
Kody Wilson <kody.wilson@oracle.com>
2022-09-21 18:21:09 -0700

    Go vars

--- t_media/vars/main.go
+++ t_media/vars/main.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	var name string = "Bob"
-	var age int = 37
+	var age int32 = 37
+	const isCool = true
 	fmt.Println(name, age)
-	fmt.Printf("name is type %T\n", name)
+	fmt.Printf("name is type %T and age is type %T\n", name, age)
+	fmt.Println("True or False, am I cool?", isCool)
 	// Primitives
 	// string
 	// bool
