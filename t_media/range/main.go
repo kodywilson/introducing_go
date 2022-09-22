b105ab021d7609792f2d2ea93654fa596fbc9f54
Kody Wilson <kodywilson@gmail.com>
2022-09-22 10:07:43 -0700

    range in Go

--- t_media/range/main.go
+++ t_media/range/main.go
@@ -6,10 +6,20 @@ func main() {
 	ids := []int{33, 76, 54, 23, 11, 2}
 
 	sum := 0
-	// Loop through ids
+	// Loop through slice of ids
 	for i, id := range ids {
 		fmt.Printf("%d - ID: %d\n", i, id)
 		sum += id
 	}
 	fmt.Println("Sum:", sum)
+
+	// Loop through map
+	books := map[string]int{
+		"Chamber of secrets": 256,
+		"Lord of the Rings":  735,
+	}
+
+	for k, v := range books {
+		fmt.Println(k, v)
+	}
 }
