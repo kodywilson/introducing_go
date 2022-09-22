fbbb628db28679fe013aa119c9d3e563067856bc
Kody Wilson <kodywilson@gmail.com>
2022-09-22 10:34:24 -0700

    closures in Go

--- t_media/pointers/main.go
+++ t_media/pointers/main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 	a := 5
-	b := &a
+	b := &a // & is the address of operator, use to get memory address
 
 	fmt.Println(a, b)
 	fmt.Printf("Type of a is %T and type of b is %T\n", a, b)
 
-	// Use * to read val from address (dereference)
+	// Use * to read val from memory address (dereference)
 	fmt.Println(*b)
 	fmt.Println(*&a)
 
