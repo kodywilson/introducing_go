3c1fc973a2efc1f248d25cb6233f6f9bfa01bac0
Kody Wilson <kody.wilson@oracle.com>
2022-09-21 18:16:02 -0700

    traversy media go crash course

---
+++ t_media/vars/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Bob"
+	var age int = 37
+	fmt.Println(name, age)
+	fmt.Printf("name is type %T\n", name)
+	// Primitives
+	// string
+	// bool
+	// int int8 int16 int32 int64
+	// uint uint8 uint16 uint32 (unsigned, always positive, no negatives)
+}
