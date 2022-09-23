47dfc3b3f846c3bfdb7eab07300bafafb79ad0f8
Kody Wilson <kodywilson@gmail.com>
2022-09-23 11:12:17 -0700

    working on nana golang booking app

--- nana_golang/booking-app/main.go
+++ nana_golang/booking-app/main.go
@@ -10,13 +10,14 @@ func main() {
 	remainingTickets := 50
 
 	fmt.Printf("Welcome to %s booking application\n", conferenceName)
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("We have a total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	var userName string
 	var userTickets int
 	//ask user for their name
-	userName = "Tom"
+	fmt.Scan(&userName)
+
 	userTickets = 2
 	fmt.Printf("User %s booked %d tickets.\n", userName, userTickets)
 
