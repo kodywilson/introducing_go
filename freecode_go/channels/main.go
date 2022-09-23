6904b65db847d5fdfd33200a49dabb125137a9d5
Kody Wilson <kodywilson@gmail.com>
2022-09-22 20:09:25 -0700

    Go channels

---
+++ freecode_go/channels/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	ch := make(chan int)
+	for j := 0; j < 5; j++ {
+		wg.Add(2)
+		go func(ch <-chan int) {
+			i := <-ch
+			fmt.Println(i)
+			wg.Done()
+		}(ch)
+		go func(ch chan<- int) {
+			ch <- 42
+			wg.Done()
+		}(ch)
+	}
+	wg.Wait()
+}
