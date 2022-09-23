6347e13d9e327b8a400fb6e8f2049b3ee167685a
Kody Wilson <kodywilson@gmail.com>
2022-09-23 11:46:05 -0700

    booking app arrays and taking user input

--- nana_golang/booking-app/main.go
+++ nana_golang/booking-app/main.go
@@ -13,12 +13,29 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var bookings = [50]string{"Bob", "Nicole", "Peter"}
+
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	//ask user for their name
-	fmt.Scan(&userName)
+	//ask user for their name and email
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets you would like to buy:")
+	fmt.Scan(&userTickets)
 
-	userTickets = 2
-	fmt.Printf("User %s booked %d tickets.\n", userName, userTickets)
+	// Confirmation message
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("User %s booked %d tickets.\n", firstName, userTickets)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Println(bookings)
 }
