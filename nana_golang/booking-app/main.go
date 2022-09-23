77132d13fc85f12c94edab5c01c4416b12347df2
Kody Wilson <kodywilson@gmail.com>
2022-09-23 16:31:42 -0700

    booking app move code into functions

--- nana_golang/booking-app/main.go
+++ nana_golang/booking-app/main.go
@@ -12,25 +12,12 @@ func main() {
 	// array to hold bookings
 	var bookings []string
 
-	fmt.Printf("Welcome to %s booking application\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	for {
 		var exit_app string
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets int
-		//ask user for their name and email
-		fmt.Println("Enter your first name:")
-		fmt.Scan(&firstName)
-		fmt.Println("Enter your last name:")
-		fmt.Scan(&lastName)
-		fmt.Println("Enter your email address:")
-		fmt.Scan(&email)
-		fmt.Println("Enter number of tickets you would like to buy:")
-		fmt.Scan(&userTickets)
+
+		firstName, lastName, email, userTickets := getUserInput(remainingTickets)
 
 		// Confirmation message
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
@@ -38,24 +25,95 @@ func main() {
 		remainingTickets = remainingTickets - userTickets
 		bookings = append(bookings, firstName+" "+lastName)
 
+		fmt.Printf("First names of those attending: %v\n", getFirstNames(bookings))
+
 		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking) // fields will split on space
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Println(firstNames)
-		fmt.Printf("These are all our bookings: %v\n", bookings)
-
+		// Leave app if tickets are all gone
 		if remainingTickets == 0 {
 			fmt.Printf("%v is sold out! Come back next year.\n", conferenceName)
 			break
 		}
+
 		fmt.Println("Would you like to exit (Y/N)?")
 		fmt.Scan(&exit_app)
 		if exit_app == "Y" || exit_app == "y" {
 			break
 		}
 	}
+}
+
+func greetUsers(conferenceName string, conferenceTickets int, remainingTickets int) {
+	fmt.Printf("Welcome to %s booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames(bookings []string) []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking) // fields will split on space
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func validateName(name string) bool {
+	if len(name) < 2 || len(name) > 32 {
+		return false
+	}
+	return true
+}
+
+func getUserInput(remainingTickets int) (string, string, string, int) {
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	//ask user for their first name, validate input
+	for {
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+		if !validateName(firstName) {
+			fmt.Printf("You entered %v, first name must be at least 2 and no more than 32 characters\n", firstName)
+		} else {
+			break
+		}
+	}
+
+	//ask user for their last name, validate input
+	for !validateName(lastName) {
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+		if !validateName(lastName) {
+			fmt.Printf("You entered %v, last name must be at least 2 and no more than 32 characters\n", lastName)
+		}
+	}
+
+	//ask for email, validate input
+	for !strings.Contains(email, "@") || !strings.Contains(email, ".") || len(email) < 5 {
+		fmt.Println("Enter your email address:")
+		fmt.Scan(&email)
+		if !strings.Contains(email, "@") || !strings.Contains(email, ".") || len(email) < 5 {
+			fmt.Printf("You entered %v, email must contain @ and . in address\n", email)
+		}
+	}
+
+	// ask for number of tickets, validate input
+	for {
+		fmt.Println("Enter number of tickets you would like to buy:")
+		fmt.Scan(&userTickets)
+
+		if userTickets > remainingTickets {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+		} else if userTickets <= 0 {
+			fmt.Printf("Please enter a non zero, non negative number up to remaining tickets: %v\n", remainingTickets)
+		} else {
+			break
+		}
+	}
+
+	return firstName, lastName, email, userTickets
+
 }
