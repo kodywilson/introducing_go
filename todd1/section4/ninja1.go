4603a841b767485a4f717c2a956927827ccbb82e
Kody Wilson <kodywilson@gmail.com>
2022-10-03 15:07:57 -0700

    section four exercise 3

--- todd1/section4/ninja1.go
+++ todd1/section4/ninja1.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 )
 
+// Ex 3
+var x int = 42
+var y string = "James Bond"
+var z bool = true
+
 func main() {
 	// Exercise 1
-	x := 42
-	y := "James Bond"
-	z := true
+	//x := 42
+	//y := "James Bond"
+	//z := true
 	fmt.Println("x, y, z are:", x, y, z)
 	fmt.Printf("x is %v of type %T\n", x, x)
 	fmt.Println(y)
 	fmt.Println(z)
+
+	// Ex 3
+	xyz := fmt.Sprintf("%v %v %v", x, y, z)
+	fmt.Println(xyz)
 
 	// Exercise 2
 	var xx int
@@ -20,4 +29,6 @@ func main() {
 	var zz bool
 	fmt.Println(xx, yy, zz)
 	// These are 0 values (default)
+
+	// Exercise 3
 }
