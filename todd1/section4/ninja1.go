5e67637a1a9ff969632650ab7b8c30ae7c7360e2
Kody Wilson <kodywilson@gmail.com>
2022-10-03 15:15:11 -0700

    section four exercise 4

--- todd1/section4/ninja1.go
+++ todd1/section4/ninja1.go
@@ -9,6 +9,11 @@ var x int = 42
 var y string = "James Bond"
 var z bool = true
 
+// Ex 4
+type number int
+
+var ex number
+
 func main() {
 	// Exercise 1
 	//x := 42
@@ -19,10 +24,6 @@ func main() {
 	fmt.Println(y)
 	fmt.Println(z)
 
-	// Ex 3
-	xyz := fmt.Sprintf("%v %v %v", x, y, z)
-	fmt.Println(xyz)
-
 	// Exercise 2
 	var xx int
 	var yy string
@@ -30,5 +31,13 @@ func main() {
 	fmt.Println(xx, yy, zz)
 	// These are 0 values (default)
 
-	// Exercise 3
+	// Ex 3
+	xyz := fmt.Sprintf("%v %v %v", x, y, z)
+	fmt.Println(xyz)
+
+	// Ex 4
+	fmt.Println("ex is", ex)
+	fmt.Printf("the type of ex is %T\n", ex)
+	ex = 42
+	fmt.Println("Now ex is", ex)
 }
