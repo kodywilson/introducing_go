eb014a757ebba50da775a66e66fbeba509e8f22f
Kody Wilson <kodywilson@gmail.com>
2022-10-04 12:22:54 -0700

    todd1 section 6 types - numbers

--- todd1/section6/main.go
+++ todd1/section6/main.go
@@ -15,4 +15,9 @@ func main() {
 	x := 42
 	y := 42.34534
 	fmt.Printf("x type is %T while y type is %T\n", x, y)
+	// int types, int8, int16, int32, int64
+	// u before int signifies unsigned (positive only)
+	// those follow the same pattern uint8, uint16, uint32, etc.
+	var bite byte = 42
+	fmt.Printf("bite is type %T\n", bite)
 }
