2e6408dd2a6b430dbd6d56befb447c9998876a78
Kody Wilson <kodywilson@gmail.com>
2022-10-07 14:45:20 -0700

    todd1 section13 - ex3-4

--- todd1/section13/main.go
+++ todd1/section13/main.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
+// ex1
 type person struct {
 	firstName     string
 	lastName      string
 	icecreamFaves []string
+}
+
+// ex3
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
 }
 
 func main() {
@@ -26,13 +43,46 @@ func main() {
 
 	// ex2 - store structs in map
 	people := map[string]person{
-		"Bob":  bob,
-		"Gary": gary,
+		bob.firstName:  bob,
+		gary.firstName: gary,
 	}
 	for k, v := range people {
 		fmt.Printf("Username: %v\nData:\n", k)
 		fmt.Println(v)
 	}
+
+	// ex3 - composite literals, embedded structs
+	greenie := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "green",
+		},
+		fourWheel: false,
+	}
+
+	redracer := sedan{
+		vehicle: vehicle{
+			doors: 4,
+			color: "red",
+		},
+		luxury: false,
+	}
+	fmt.Println(greenie)
+	fmt.Println(redracer)
+	fmt.Println(greenie.doors, greenie.fourWheel, redracer.color, redracer.luxury)
+
+	// ex4 - anonymous struct
+	anon := struct {
+		mappy  map[string]int
+		slicey []string
+	}{
+		mappy: map[string]int{
+			"age": 34,
+		},
+		slicey: []string{"beer", "cheese"},
+	}
+	fmt.Println(anon)
+	fmt.Println(anon.slicey)
 }
 
 // ex 1
