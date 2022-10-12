168f042ec731262432d1a66ead7a236b21d94c93
Kody Wilson <kodywilson@gmail.com>
2022-10-11 17:34:42 -0700

    todd1 section14 - interfaces

--- todd1/section14/main.go
+++ todd1/section14/main.go
@@ -15,7 +15,20 @@ type secretAgent struct {
 // method signature
 // func (r receiver) identfier(parameters) (return(s)) { code }
 func (s secretAgent) speak() {
-	fmt.Println("I am", s.first, s.last)
+	fmt.Println("I am", s.first, s.last, "I'm not a secret agent.")
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first, p.last, "I am a secret agent.")
+}
+
+// interfaces allow us to define behavior and use polymorphism
+type human interface {
+	speak()
+}
+
+func talk(h human) {
+	h.speak()
 }
 
 // attach to receiver (secretAgent in this case)
@@ -48,6 +61,15 @@ func main() {
 	fmt.Println(sa1)
 	sa1.speak()
 	sa2.speak()
+
+	p1 := person{
+		first: "Dr",
+		last:  "No",
+	}
+
+	fmt.Println(p1)
+	talk(p1)
+	talk(sa1)
 }
 
 // func (r receiver) identifier(parameters) (return(s)) { ... }
