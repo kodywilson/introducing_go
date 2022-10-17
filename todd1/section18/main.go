baff5f982429d99931227bb223645e3d682c2d65
Kody Wilson <kodywilson@gmail.com>
2022-10-17 11:42:32 -0700

    todd1 section 18 - applications - json

--- todd1/section18/main.go
+++ todd1/section18/main.go
@@ -11,6 +11,12 @@ type person struct {
 	Age   int
 }
 
+type parson struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
+
 func main() {
 	// marshall
 	p1 := person{
@@ -33,4 +39,15 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(bs))
+
+	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Money","Last":"Penny","Age":27}]`
+	bes := []byte(s)
+
+	// slice of parsons to store my unmarshalled json
+	var peeps []parson
+	erro := json.Unmarshal(bes, &peeps)
+	if erro != nil {
+		fmt.Println(erro)
+	}
+	fmt.Println(peeps)
 }
