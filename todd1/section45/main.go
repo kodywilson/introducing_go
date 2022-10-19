d69d842c47c48ecbbcaf18cab0a3140ccd828b56
Kody Wilson <kodywilson@gmail.com>
2022-10-19 12:48:29 -0700

    todd1 section 44 - channels

--- todd1/section45/main.go
+++ todd1/section45/main.go
@@ -2,32 +2,89 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	c := make(chan int) // unbuffered channel, blocks
+	// c := make(chan int) // unbuffered channel, blocks
 
-	// first run with listening function instead of range
+	// // first run with listening function instead of range
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		c <- i
+	// 	}
+	// }()
+
+	// go func() {
+	// 	for {
+	// 		fmt.Println(<-c)
+	// 	}
+	// }()
+
+	// time.Sleep(time.Second)
+	// fmt.Println("Done with first round")
+
+	// // now, use range this time
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		c <- i
+	// 	}
+	// 	close(c)
+	// }()
+
+	// for n := range c {
+	// 	fmt.Println(n)
+	// }
+
+	// fmt.Println("Done with second round")
+	// // n-1 - didn't work, trying semaphore
+	// cha := make(chan int)
+	// done := make(chan bool)
+	// // create wait group
+	// //var wg sync.WaitGroup
+
+	// //wg.Add(2)
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		cha <- i
+	// 	}
+	// 	//wg.Done()
+	// 	done <- true
+	// }()
+
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		cha <- i
+	// 	}
+	// 	//wg.Done()
+	// 	done <- true
+	// }()
+
+	// go func() {
+	// 	//wg.Wait()
+	// 	<-done // you can throw values away off channel
+	// 	<-done
+	// 	close(cha)
+	// }()
+
+	// for num := range cha {
+	// 	fmt.Println(num)
+	// }
+	n := 10
+	c := make(chan int)
+	done := make(chan bool)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for i := 0; i < 10; i++ {
+				c <- i
+			}
+			done <- true
+		}()
+	}
+
 	go func() {
-		for i := 0; i < 10; i++ {
-			c <- i
-		}
-	}()
-
-	go func() {
-		for {
-			fmt.Println(<-c)
-		}
-	}()
-
-	time.Sleep(time.Second)
-	fmt.Println()
-
-	// now, use range this time
-	go func() {
-		for i := 0; i < 10; i++ {
-			c <- i
+		for i := 0; i < n; i++ {
+			<-done
 		}
 		close(c)
 	}()
