7e2689ca281385da9f0f97cf8751f96f79bc0dfd
Kody Wilson <kodywilson@gmail.com>
2022-10-25 14:39:40 -0700

    todd2 section 1 - review

--- todd2/section1/main.go
+++ todd2/section1/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+type secretAgent struct {
+	person
+	licenseToKill bool
+}
+
+// keyword (receiver) name(arguments) (return) {func body}
+func (p person) speak() {
+	fmt.Println(p.first, "says Hello")
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println(sa.first, "says, I am not an agent.")
+}
+
 func main() {
 	x := 7
 	fmt.Printf("Type of x is %T\n", x)
@@ -16,4 +36,23 @@ func main() {
 		"Charles": 65,
 	}
 	fmt.Println(m)
+
+	// struct
+	p1 := person{
+		first: "Bob",
+		last:  "Johnson",
+		age:   25,
+	}
+	fmt.Println(p1)
+	p1.speak()
+
+	sa1 := secretAgent{
+		person{
+			"James",
+			"Bond",
+			32,
+		},
+		true,
+	}
+	sa1.speak()
 }
