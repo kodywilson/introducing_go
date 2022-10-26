78af4016509495bafae25cd89fb771b3001648ac
Kody Wilson <kodywilson@gmail.com>
2022-10-26 16:13:26 -0700

    todd2 section 2 - templates - struct with slices of struct

---
+++ todd2/section2/007/struct-slice-struct/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+type items struct {
+	Wisdom    []sage
+	Transport []car
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	gandhi := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
+	}
+
+	mlk := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed",
+	}
+
+	f := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+
+	c := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
+	sages := []sage{buddha, gandhi, mlk}
+	cars := []car{f, c}
+
+	data := items{
+		Wisdom:    sages,
+		Transport: cars,
+	}
+
+	err := tpl.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
