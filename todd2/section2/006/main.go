fdbcaa9a6bf23a153a5222c19bed9c6c1df0b0b6
Kody Wilson <kodywilson@gmail.com>
2022-10-26 09:26:19 -0700

    todd2 section 2 - templates - data in

--- todd2/section2/006/main.go
+++ todd2/section2/006/main.go
@@ -6,29 +6,29 @@ import (
 	"text/template"
 )
 
-func main() {
-	// parse one file into tpl
-	tpl, err := template.ParseGlob("templates/*.gmao")
-	if err != nil {
-		log.Fatalln(err)
-	}
+var tpl *template.Template
 
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
 	// show contents of tpl (only shows first one added)
-	err = tpl.Execute(os.Stdout, nil)
+	err := tpl.Execute(os.Stdout, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	// execute each template individually
-	err = tpl.ExecuteTemplate(os.Stdout, "one.gmao", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = tpl.ExecuteTemplate(os.Stdout, "two.gmao", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gmao", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
---
+++ todd2/section2/007/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl template.Template
+
+func init() {
+	tpl = *template.Must(template.ParseGlob("tpl.gohtml"))
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", `Release self-focus, embrace other-focus.`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
