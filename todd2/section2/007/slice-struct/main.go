11c8f42307b0f2ef7a5b88e62759a991cf6ecc69
Kody Wilson <kodywilson@gmail.com>
2022-10-26 15:55:50 -0700

    todd2 section 2 - templates - slice of structs

---
+++ todd2/section2/007/slice-struct/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	gandhi := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
+	}
+
+	mlk := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed",
+	}
+
+	sages := []sage{buddha, gandhi, mlk}
+
+	err := tpl.Execute(os.Stdout, sages)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
