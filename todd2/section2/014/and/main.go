3235ea7080fe63b40d97f73c1cb580aaf9b3aa5c
Kody Wilson <kodywilson@gmail.com>
2022-10-27 15:23:45 -0700

    todd2 section 2 - templates - and

--- todd2/section2/014/and/main.go
+++ todd2/section2/014/and/main.go
@@ -4,28 +4,44 @@ import (
 	"log"
 	"os"
 	"text/template"
-	"time"
 )
 
 // don't forget this is a pointer to template.Template!
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
-// create FuncMap
-var fm = template.FuncMap{
-	"fdateMDY": monthDayYear,
-}
-
-func monthDayYear(t time.Time) string {
-	return t.Format("01-02-2006")
+type user struct {
+	Name  string
+	Motto string
+	Admin bool
 }
 
 func main() {
 
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
+	u1 := user{
+		Name:  "Joe",
+		Motto: "Mama",
+		Admin: true,
+	}
+
+	u2 := user{
+		Name:  "George",
+		Motto: "I'd rather be lucky than good",
+		Admin: true,
+	}
+
+	u3 := user{
+		Name:  "Bob",
+		Motto: "I like apples",
+		Admin: true,
+	}
+
+	users := []user{u1, u2, u3}
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", users)
 	if err != nil {
 		log.Fatalln(err)
 	}
