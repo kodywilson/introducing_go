075e78404f124239328979eb14f659bd6fbb5631
Kody Wilson <kodywilson@gmail.com>
2022-11-01 18:01:29 -0700

    snippetbox add static resources

--- lets_go/snippetbox/cmd/web/main.go
+++ lets_go/snippetbox/cmd/web/main.go
@@ -9,7 +9,11 @@ func main() {
 	// Initialize a new servemux
 	mux := http.NewServeMux()
 
+	// create file server for statics
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	// Register handlers
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)     // register snippet view handler
 	mux.HandleFunc("/snippet/create", snippetCreate) // register snippet create handler
