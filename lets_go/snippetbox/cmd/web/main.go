00bb9e8b2f0791531c1dc522a39f86a1bbef6f11
Kody Wilson <kodywilson@gmail.com>
2022-11-02 12:02:36 -0700

    set up command line args with defaults

--- lets_go/snippetbox/cmd/web/main.go
+++ lets_go/snippetbox/cmd/web/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+	// set up config via command line arguments or use defaults
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "Web server port")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
 	// Initialize a new servemux
 	mux := http.NewServeMux()
 
 	// create file server for statics
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 
 	// Register handlers
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
@@ -19,7 +31,7 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate) // register snippet create handler
 
 	// Start web server
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s\n", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
