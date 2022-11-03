0721b5be32e4c8553f6dbad10c01f509d0446c05
Kody Wilson <kodywilson@gmail.com>
2022-11-02 17:32:17 -0700

    snippets add db driver and open connection pool

--- lets_go/snippetbox/cmd/web/main.go
+++ lets_go/snippetbox/cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +17,7 @@ type application struct {
 
 type config struct {
 	addr      string
+	dsn       string
 	staticDir string
 }
 
@@ -21,12 +25,20 @@ func main() {
 	// set up config via command line arguments or use defaults
 	var cfg config
 	flag.StringVar(&cfg.addr, "addr", ":4000", "Web server port")
+	flag.StringVar(&cfg.dsn, "dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
 
 	// set up loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// create DB connection pool
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// initialize app struct
 	app := &application{
@@ -43,6 +55,17 @@ func main() {
 
 	infoLog.Printf("Starting server on %s\n", cfg.addr)
 	// call ListenAndServe() on srv http struct
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
