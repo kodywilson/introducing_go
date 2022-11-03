824cbf864a6bc092ece205009b15d6318e6cb68b
Kody Wilson <kodywilson@gmail.com>
2022-11-03 14:51:25 -0700

    greenlight - first commit

---
+++ lets_go/greenlight/cmd/api/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// later auto generate from build
+const version = "1.0.0"
+
+// configuration struct
+type config struct {
+	port int
+	env  string
+}
+
+// application struct
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	// declare cfg, config struct
+	var cfg config
+
+	// read command line arguments or supply defaults
+	flag.IntVar(&cfg.port, "port", 7000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// initialize logger
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// declare app of type application struct
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// declare servemux and add routes
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// declare HTTP server and set reasonable defaults
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// start HTTP server
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
