9a640778a2e09a690a1dd98ce3501be9d328166a
Kody Wilson <kody.wilson@oracle.com>
2022-11-05 23:40:28 -0700

    snippetbox - form dat

--- lets_go/snippetbox/cmd/web/handlers.go
+++ lets_go/snippetbox/cmd/web/handlers.go
@@ -12,6 +12,14 @@ import (
 	"github.com/kodywilson/snippetbox/internal/models"
 )
 
+// store snippet form info
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
+
 // Home handler function
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// generate list of most recent snippets
@@ -62,20 +70,20 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
   data := app.newTemplateData(r)
 
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
+
 	app.render(w, http.StatusOK, "create.gohtml", data)
 }
 
-// snippetCreate handler function
+// snippetCreatePost POST new snippet
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 			app.clientError(w, http.StatusBadRequest)
 			return
 	}
-
-  // Get the values with the matching keys (title, content, etc.)
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
 
 	// form data is string, but we want int for expires
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
@@ -84,34 +92,41 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 			return
 	}
 
-	// validate data
-	fieldErrors := make(map[string]string)
+	// Allows us to store form data in case user has to submit again
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+  }
 
 	// check title length
-	if strings.TrimSpace(title) == "" {
-		fieldErrors["title"] = "This field cannot be blank"
-  } else if utf8.RuneCountInString(title) > 100 {
-		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+  } else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
   }
 
 	// check content field
-	if strings.TrimSpace(content) == "" {
-		fieldErrors["content"] = "This field cannot be blank"
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
   }
 
 	// check expires
-	if expires != 1 && expires != 7 && expires != 365 {
-		fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
   }
 
 	// return errors if any are found
-	if len(fieldErrors) > 0 {
-		fmt.Fprint(w, fieldErrors)
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.gohtml", data)
 		return
 	}
 
   // write the snippet data to the DB
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 			app.serverError(w, err)
 			return
