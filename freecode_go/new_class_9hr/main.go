1619c3ddddc8736b2d1ce8d66a2ba9ab35a724b1
Kody Wilson <kodywilson@gmail.com>
2023-08-10 21:21:34 -0700

    freecode 9hr fix bug

--- freecode_go/new_class_9hr/main.go
+++ freecode_go/new_class_9hr/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to textio!")
+	fmt.Println("Starting Textio server")
+  messagesFromDoris := []string{
+    "You doing anything later??",
+    "Did you get my last message?",
+    "Don't leave me hanging...",
+    "Please respond",
+  }
+  numMessages := float64(len(messagesFromDoris))
+  costPerMessage := .02
+
+  totalCost := costPerMessage * numMessages
+
+  fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
 }
